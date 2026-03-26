dc831a8153a678bb7034a39ddf31f48372726fc1
Bereket-Ketema <bereketketema866@gmail.com>
2026-03-26 23:17:39 +0300

    delete functionality have been added

--- components/NoteItem.tsx
+++ components/NoteItem.tsx
@@ -3,19 +3,28 @@ import { Note } from '../types';
 import { useRouter } from 'expo-router';
 import React from 'react';
 
+
 type Props = {
   note: Note;
   onDelete: () => void;
+  onToggleFavorite: () => void;
 };
 
-export default function NoteItem({ note, onDelete }: Props) {
+export default function NoteItem({ note, onDelete, onToggleFavorite }: Props) {
   const router = useRouter();
 
   return (
     <View style={styles.item}>
-      <Text>{note.text}</Text>
+      <Text>{note.text} ({note.category})</Text>
+      <Text>
+        {note.isFavorite ? '⭐ ' : ''}{note.text}
+      </Text>
 
       <View style={{ flexDirection: 'row', gap: 10 }}>
+        <Pressable onPress={onToggleFavorite}>
+          <Text style={{ color: 'gold' }}>★</Text>
+        </Pressable>
+
         <Pressable onPress={() => router.push(`/edit/${note.id}`)}>
           <Text style={{ color: 'blue' }}>Edit</Text>
         </Pressable>
