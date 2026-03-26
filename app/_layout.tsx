ace1760c48371f3435d9d048abb5d1dc9a0b6899
Bereket-Ketema <bereketketema866@gmail.com>
2026-03-26 22:41:58 +0300

    the upper layout storage async have been added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,27 @@
 import { Stack } from 'expo-router';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Note } from '../types';
 import { createContext } from 'react';
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const NotesContext = createContext<any>(null);
 
 export default function Layout() {
   const [notes, setNotes] = useState<Note[]>([]);
+
+  // 🔹 Load notes on start
+  useEffect(() => {
+    const loadNotes = async () => {
+      const data = await AsyncStorage.getItem('notes');
+      if (data) setNotes(JSON.parse(data));
+    };
+    loadNotes();
+  }, []);
+
+  // 🔹 Save notes whenever changed
+  useEffect(() => {
+    AsyncStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
 
   return (
     <NotesContext.Provider value={{ notes, setNotes }}>
